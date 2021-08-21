6b9ab60cabe5e5bcb8673f53d05c1bb855614403
Devang Saklani <72780418+Devang47@users.noreply.github.com>
2021-08-21 13:37:03 +0530

    minor changes

--- src/script.js
+++ src/script.js
@@ -1,4 +1,5 @@
-import "./style.scss";
+import "./CSS/normalize.css";
+import "./CSS/style.scss";
 
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
